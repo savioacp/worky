5ae14a1a00bc377f701feaa003508818078c0f35
Sávio Pereira <savioacp@gmail.com>
2021-09-07 16:05:03 -0300

    Create WorkerThread

---
+++ worky/worker/__init__.py
@@ -0,0 +1,4 @@
+from .worker_thread import WorkerThread
+from .stateless_worker import StatelessWorker
+
+__all__ = [WorkerThread, StatelessWorker]
---
+++ worky/worker/worker_thread.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from queue import Queue
+import threading
+from worky.worker.work_unit import Break
+
+
+class WorkerThread(threading.Thread, ABC):
+    """
+    Defines abstract, base class for worker threads.
+
+    Every thread should have 
+    - a setup function, that will execute when the worker is created;
+    - a work function, that will execute every time a unit of
+    work arrives;
+    - a cleanup function, that will execute when there aren't
+    any more units of work, or if work fails.  
+    """
+
+    def __init__(self, queue):
+        super().__init__()
+        self.queue: Queue = queue
+        self.done = False
+
+    @abstractmethod
+    def setup(self):
+        """
+        Happens before the worker starts working.
+        """
+        pass
+
+    @abstractmethod
+    def work(self, unit):
+        """
+        Do the work.
+
+        Parameters
+        ---
+        - unit - The unit of work.
+        """
+        pass
+
+    @abstractmethod
+    def cleanup(self):
+        """
+        Happens before the worker stops working.
+        """
+        pass
+
+    def run(self) -> None:
+        self.setup()
+
+        try:
+            while True:
+                unit = self.queue.get()
+                if unit is Break:
+                    self.queue.task_done()
+                    break
+                else:
+                    self.work(unit)
+                
+                self.queue.task_done()
+        finally:
+            self.cleanup()
+
+    def stop(self):
+        self.done = True
